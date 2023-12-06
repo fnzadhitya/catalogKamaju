5b0d524d6840176da5988d6e90b2bb4c033efb58
Holligans22 <137614234+Holligans22@users.noreply.github.com>
2023-12-06 15:13:04 +0700

    Holligansh22

--- catalogue_admin/conf/api.php
+++ catalogue_admin/conf/api.php
@@ -23,17 +23,17 @@ while ($row = $resultAPD->fetch_assoc()) {
 $customerId = $_GET['customer_id'];
 $jobId = $_GET['job_id'];
 
-// Filter data seragam
+
 $filteredSeragam = array_filter($dataSeragam, function ($item) use ($customerId, $jobId) {
     return $item['id_customer'] == $customerId && $item['id_job'] == $jobId;
 });
 
-// Filter data APD
+
 $filteredAPD = array_filter($dataAPD, function ($item) use ($customerId, $jobId) {
     return $item['id_customer'] == $customerId && $item['id_job'] == $jobId;
 });
 
-// Tutup koneksi database
+
 $conn->close();
 
 echo json_encode(['seragam' => array_values($filteredSeragam), 'apd' => array_values($filteredAPD)]);
