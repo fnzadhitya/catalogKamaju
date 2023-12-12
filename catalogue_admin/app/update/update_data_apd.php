f22602909362d5b073531be2893b4ee04be602b8
fnzadhitya <137802138+fnzadhitya@users.noreply.github.com>
2023-12-12 08:40:58 +0700

    Update update_data_apd.php

--- catalogue_admin/app/update/update_data_apd.php
+++ catalogue_admin/app/update/update_data_apd.php
@@ -10,18 +10,19 @@ $type = $_POST['type'];
 $merk = $_POST['merk'];
 $warna = $_POST['warna'];
 
-// Retrieve existing photo names from the database
 $query_get_photos = mysqli_query($koneksi, "SELECT foto FROM tb_apd WHERE id_apd='$id'");
 $existing_photos = mysqli_fetch_assoc($query_get_photos);
 
-// Proses untuk foto
-$foto = $existing_photos['foto'];
 if (!empty($_FILES['foto']['name'])) {
+    if (!empty($_POST['hapus_foto']) && $_POST['hapus_foto'] == 'on') {
+        unlink('../foto/' . $existing_photos['foto']);
+        $existing_photos['foto'] = null;
+    }
+
     $foto  = $_FILES['foto']['name'];
     $file_tmp = $_FILES['foto']['tmp_name'];
     move_uploaded_file($file_tmp, '../foto/' . $foto);
 }
-
 
 $query = $koneksi->prepare("UPDATE tb_apd SET nama_apd=?, foto=?, id_customer=?, id_job=?, jumlah=?, type=?, merk=?, warna=? WHERE id_apd=?");
 $query->bind_param("ssiiisssi", $nama, $foto, $customer, $job, $jumlah, $type, $merk, $warna, $id);
