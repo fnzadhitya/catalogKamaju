171a882b5a64f90a2fd1842f1f2613e313f4e2d1
fnzadhitya <137802138+fnzadhitya@users.noreply.github.com>
2023-12-14 10:16:55 +0700

    Update update_data_apd.php

--- catalogue_admin/app/update/update_data_apd.php
+++ catalogue_admin/app/update/update_data_apd.php
@@ -13,15 +13,25 @@ $warna = $_POST['warna'];
 $query_get_photos = mysqli_query($koneksi, "SELECT foto FROM tb_apd WHERE id_apd='$id'");
 $existing_photos = mysqli_fetch_assoc($query_get_photos);
 
-if (!empty($_FILES['foto']['name'])) {
-    if (!empty($_POST['hapus_foto']) && $_POST['hapus_foto'] == 'on') {
-        unlink('../foto/' . $existing_photos['foto']);
-        $existing_photos['foto'] = null;
-    }
+// Inisialisasi foto dengan foto yang sudah ada
+$foto = $existing_photos['foto'];
 
-    $foto  = $_FILES['foto']['name'];
-    $file_tmp = $_FILES['foto']['tmp_name'];
-    move_uploaded_file($file_tmp, '../foto/' . $foto);
+// Cek apakah ada foto baru diunggah
+if (!empty($_FILES['foto']['name'])) {
+    // Hapus foto lama jika dicentang
+    if (!empty($_POST['hapus_foto']) && $_POST['hapus_foto'] == 'on' && !empty($existing_photos['foto'])) {
+        unlink('../foto/' . $existing_photos['foto']);
+        $foto = null;
+    } else {
+        // Jika tidak dicentang atau tidak ada foto lama, ganti foto dengan yang baru diunggah
+        $foto  = $_FILES['foto']['name'];
+        $file_tmp = $_FILES['foto']['tmp_name'];
+        move_uploaded_file($file_tmp, '../foto/' . $foto);
+    }
+} elseif (!empty($_POST['hapus_foto']) && $_POST['hapus_foto'] == 'on' && !empty($existing_photos['foto'])) {
+    // Hapus foto tanpa menggantinya dengan yang baru jika checkbox dicentang
+    unlink('../foto/' . $existing_photos['foto']);
+    $foto = null;
 }
 
 $query = $koneksi->prepare("UPDATE tb_apd SET nama_apd=?, foto=?, id_customer=?, id_job=?, jumlah=?, type=?, merk=?, warna=? WHERE id_apd=?");
