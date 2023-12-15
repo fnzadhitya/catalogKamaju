2e6790d90d92547c2186debf4b10f2670ac2783f
fnzadhitya <137802138+fnzadhitya@users.noreply.github.com>
2023-12-15 07:37:47 +0700

    Update api.php

--- catalogue_admin/conf/api.php
+++ catalogue_admin/conf/api.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: *");
 
 $conn = new mysqli("localhost", "root", "", "db_catalog");
